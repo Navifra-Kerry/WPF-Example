d71b392d57335e60c7a012dcd2d3867b01474d24
kerry.cho <cdh0012@inveniacorp.com>
2019-07-23 09:38:45 +0900

    Add OpenFileDialog

---
+++ Dialog/OpenFileDialogSample/OpenFileDialogMultipleFilesSample/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+
+
+namespace OpenFileDialogMultipleFilesSample
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFiles_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (string filename in openFileDialog.FileNames)
+                    lbFiles.Items.Add(Path.GetFileName(filename));
+            }
+        }
+    }
+}
---
+++ Dialog/OpenFileDialogSample/OpenFileDialogSample/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace OpenFileDialogSample
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+                txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+    }
+}
